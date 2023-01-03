02567ed1e8b07b9de74a45af3e0c462640dd4cac
aymaneE <aymane.elmouhtarim@particeep.com>
2023-01-03 08:27:06 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'exam-attendances',
+    appId: '1:875528858462:web:5bb378688adfd2650c3dcf',
+    databaseURL: 'https://exam-attendances-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'exam-attendances.appspot.com',
+    apiKey: 'AIzaSyDHp8SBlp_XmfKBLiCbP5m9hFmNH7MviHk',
+    authDomain: 'exam-attendances.firebaseapp.com',
+    messagingSenderId: '875528858462',
+  },
+    production: false
+};
